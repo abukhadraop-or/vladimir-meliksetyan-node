54f0cb5434230b5fee03c074beed0baff14f8c03
Vladimir-OP <vladimir.meliksetyan@optimumpartners.co>
2022-10-06 13:53:20 +0400

    minor changes

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -8,17 +8,18 @@ const { User } = require("../models");
  * @param {HTTP request} req
  * @param {HTTP response} res
  */
-const registerUser = async (req, res) => { 
+const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
   const id = uuid();
   // hash password
   const hashedPassword = await bcrypt.hash(password, 10);
   // check does user exist with given email or not
-  const existeingUser = await User.findOne({ where: { email } });
-  if (existeingUser) {
-    res.status(409).send("User Already exists with this email");
-  } else {
-    try {
+
+  try {
+    const existeingUser = await User.findOne({ where: { email } });
+    if (existeingUser) {
+      res.status(409).send("User Already exists with this email");
+    } else {
       const result = await User.create({
         id,
         username,
@@ -26,9 +27,9 @@ const registerUser = async (req, res) => {
         password: hashedPassword,
       });
       res.status(201).send("user Created");
-    } catch (error) {
-      res.send(error.message).status(400);
     }
+  } catch (error) {
+    res.send(error.message).status(400);
   }
 };
 
@@ -40,18 +41,23 @@ const registerUser = async (req, res) => {
 const userLogin = async (req, res) => {
   const { email, password } = req.body;
   // find user with email
-  const user = await User.findOne({ where: { email } });
-  // authorize user
-  const foundUser = user?.dataValues;
-
-  // check password validation
-  if (foundUser) {
-    const checkpassword = await bcrypt.compare(password, user.password);
-    const token = jwt.sign(foundUser, process.env.JWT_ACCESS_SECRET);
-    if (checkpassword) {
-      res.status(200).send({ token });
-    } else res.send("wrong password");
-  } else res.status(404).send("user does not exist");
+  try {
+    const user = await User.findOne({ where: { email } });
+    // authorize user
+    const foundUser = user?.dataValues;
+    // check password validation
+    if (foundUser) {
+      const checkpassword = await bcrypt.compare(password, user.password);
+      const token = jwt.sign(foundUser, process.env.JWT_ACCESS_SECRET);
+      if (checkpassword) {
+        res.status(200).send({ token });
+      } else res.send("wrong password");
+    } else res.status(404).send("user does not exist");
+  } catch (error) {
+    console.log(error);
+    res.json({error:error.message}).status(401)
+  }
+ 
 };
 
 module.exports = {
